1f574eff100b2c670b21bca86f7232cc44f6fa44
joshsprague <jjspragu@asu.edu>
2013-11-06 23:28:29 -0800

    Setup player start point and movement

--- prioryrl.py
+++ prioryrl.py
@@ -5,11 +5,29 @@ SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'priory', False)
-
 libtcod.sys_set_fps(LIMIT_FPS)
 
+playerx = SCREEN_WIDTH/2
+playery = SCREEN_HEIGHT/2
+
+def handle_keys():
+  global playerx, playery
+
+  #movement keys
+  if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+    playery -= 1
+
+  elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+    playery += 1
+
+  elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+    playerx -= 1
+
+  elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+    playerx += 1
+    
+# main loop
 while not libtcod.console_is_window_closed():
   libtcod.console_set_default_foreground(0, libtcod.white)
   libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
